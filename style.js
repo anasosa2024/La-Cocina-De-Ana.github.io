9329187dfe3eaa19644d0357900d6270a3bd233c
anasosa2024 <162615922+anasosa2024@users.noreply.github.com>
2024-04-04 23:31:17 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,4 @@
+function mostrarMenu() {
+    const opcionesMenu = document.querySelector('.opciones-menu');
+    opcionesMenu.style.display = opcionesMenu.style.display === 'none' ? 'block' : 'none';
+  }
